8353497054f08a8b434c167b23e815aead02bee3
Sean Goedecke <sgoedecke@zendesk.com>
2019-03-17 16:25:08 +1100

    go fmt

--- main.go
+++ main.go
@@ -1,62 +1,60 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "github.com/appleboy/easyssh-proxy"
-  "github.com/kevinburke/ssh_config"
-  "time"
-  "os"
-  "os/user"
+	"fmt"
+	"github.com/appleboy/easyssh-proxy"
+	"github.com/kevinburke/ssh_config"
+	"log"
+	"os"
+	"os/user"
+	"time"
 )
 
 func main() {
-    u, _ := user.Current()
-    user := u.Username
-    log.Print("Executing as " + user)
+	u, _ := user.Current()
+	user := u.Username
+	log.Print("Executing as " + user)
 
-    host := os.Args[1]
-    targetServer := ssh_config.Get(host, "HostName")
-    bastionNickname := ssh_config.Get(host, "ProxyJump")
-    bastionServer := ssh_config.Get(bastionNickname, "HostName")
+	host := os.Args[1]
+	targetServer := ssh_config.Get(host, "HostName")
+	bastionNickname := ssh_config.Get(host, "ProxyJump")
+	bastionServer := ssh_config.Get(bastionNickname, "HostName")
 
 	ssh := &easyssh.MakeConfig{
-		User:    user,
-		Server:  targetServer,
-		Port:    "22",
+		User:   user,
+		Server: targetServer,
+		Port:   "22",
 		Proxy: easyssh.DefaultConfig{
-			User:    user,
-			Server:  bastionServer,
-			Port:    "22",
+			User:   user,
+			Server: bastionServer,
+			Port:   "22",
 		},
 	}
 
-  hostname, _, _, err := ssh.Run("hostname")
-  if err != nil {
-    log.Fatal("Failed to run: " + err.Error())
-  }
-  log.Print("Executing on " + hostname)
+	hostname, _, _, err := ssh.Run("hostname")
+	if err != nil {
+		log.Fatal("Failed to run: " + err.Error())
+	}
+	log.Print("Executing on " + hostname)
 
+	datePath := time.Now().Format("2006/01/02")
 
-  datePath := time.Now().Format("2006/01/02")
+	searchGlob := os.Args[2]
+	searchKey := os.Args[3]
 
-  searchGlob := os.Args[2]
-  searchKey := os.Args[3]
+	if searchGlob == "" {
+		log.Fatal("You need to pass a glob")
+	}
+	if searchKey == "" {
+		log.Fatal("You need to pass a search key")
+	}
 
-  if searchGlob == "" {
-    log.Fatal("You need to pass a glob")
-  }
-  if searchKey == "" {
-    log.Fatal("You need to pass a search key")
-  }
+	cmd := fmt.Sprintf("cat /storage/logs/hosts/%s/%s | grep %s", datePath, searchGlob, searchKey)
+	log.Print(cmd)
+	stdout, _, _, err := ssh.Run(cmd)
+	if err != nil {
+		log.Fatal("Failed to run: " + err.Error())
+	}
 
-  cmd := fmt.Sprintf("cat /storage/logs/hosts/%s/%s | grep %s", datePath, searchGlob, searchKey)
-  log.Print(cmd)
-  stdout, stderr, _, err := ssh.Run(cmd)
-  if err != nil {
-    log.Print("Stderr: " + stderr + stdout)
-    log.Fatal("Failed to run: " + err.Error())
-  }
-
-  log.Print(stdout)
+	log.Print(stdout)
 }
